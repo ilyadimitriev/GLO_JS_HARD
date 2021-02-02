54e47ab2fc4566adf37030d59b28392d55afe17b
Илья Димитриев <idimitriev123@gmail.com>
2021-02-02 17:14:59 +1000

    Выполнил усложненное задание 4-го урока

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function doJS(glo){
+    if (typeof glo !== 'string') {
+        return 'Внимание! Переданный функции doJS() аргумент не является сторокой.';
+    }
+    else {
+        glo = glo.trim();
+        if (glo.length > 30) {
+            return glo.substring(0, 29) + '...';
+        }
+        else {
+            return glo;
+        }
+    }
+}
+
+alert(doJS(prompt('Введите любой текст:', '   Самое частое нарушение среди пешеходов – переход проезжей части вне установленных мест (69% случаев). ')));

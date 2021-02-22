d2e57fd1e55428a1e63825f24d9aec7acf8d5720
Илья Димитриев <idimitriev123@gmail.com>
2021-02-22 18:34:16 +1000

    Выполнил первую часть Урока 18

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/* eslint-disable indent */
+`use strict`;
+
+window.addEventListener(`DOMContentLoaded`, () => {
+
+function countTimer(deadline) {
+
+	const timerHours = document.querySelector(`#timer-hours`),
+		timerMinutes = document.querySelector(`#timer-minutes`),
+		timerSeconds = document.querySelector(`#timer-seconds`);
+	let timer = getTimeRemaining();
+
+	function addZeros(num) {
+		num = num.toString();
+		return (num.length <= 1) ? `0` + num : num;
+	}
+
+	function getTimeRemaining() {
+		const dateStop = new Date(deadline).getTime(),
+			dateNow = new Date().getTime(),
+			timeRemaining = (dateStop - dateNow) / 1000,
+			seconds = addZeros(Math.floor(timeRemaining % 60)),
+			minutes = addZeros(Math.floor((timeRemaining / 60) % 60)),
+			hours = addZeros(Math.floor(timeRemaining / 60 / 60));
+		return { timeRemaining, hours, minutes, seconds };
+	}
+
+	function updateClock() {
+		timer = getTimeRemaining();
+		timerHours.textContent = timer.hours;
+		timerMinutes.textContent = timer.minutes;
+		timerSeconds.textContent = timer.seconds;
+	}
+	if (timer.timeRemaining > 0) {
+		setInterval(updateClock, 1000);
+	} else {
+		timerHours.textContent = `00`;
+		timerMinutes.textContent = `00`;
+		timerSeconds.textContent = `00`;
+	}
+}
+
+const deadline = new Date().getTime() + 23000000;
+countTimer(deadline);
+
+});

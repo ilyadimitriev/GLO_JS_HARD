9de264d3fd8ce306661196b0c16e72c58e703d17
Ilya <idimitiriev123@gmail.com>
2021-03-02 00:25:25 +1000

    Выполнил Урок 23

--- js/main.js
+++ js/main.js
@@ -257,4 +257,77 @@ const  slider = () => {
 };
 slider();
 
+	// Наведение на фото команды
+const changeCommandPhoto = () => {
+	const command = document.querySelectorAll(`.command__photo`);
+	const togglePhoto = event => {
+			const altImgSrc = event.target.getAttribute(`src`);
+			event.target.setAttribute(`src`, event.target.dataset.img);
+			event.target.dataset.img = altImgSrc;
+	};
+	command.forEach(commandMember => {
+		commandMember.addEventListener(`mouseenter`, togglePhoto);
+		commandMember.addEventListener(`mouseleave`, togglePhoto);
+	});
+};
+changeCommandPhoto();
+
+	// Ограничение на ввод символов в полях ввода имени, сообщения, телефона, эл. почты и данных калькулятора
+const restrictInput = () => {
+	document.addEventListener(`input`, event => {
+		const target = event.target.closest(`input`);
+
+		if (target.classList.contains(`calc-item`)) {
+			target.value = target.value.replace(/[^\d]/g, ``);
+			return;
+		}
+		switch (target.getAttribute(`name`)) {
+			case `user_name`:
+			case `user_message`:
+				target.value = target.value.replace(/[^а-яё -]/gi, ``);
+				break;
+			case `user_email`:
+				target.value = target.value.replace(/[^\w'*~!.@-]/gi, ``);
+				break;
+			case 'user_phone':
+				target.value = target.value.replace(/[^\d()-]/gi, ``);
+				break;
+		}
+	});
+};
+restrictInput();
+
+	// Проверка и приведение введенных данных на валидность
+const checkInputData = () => {
+	document.addEventListener(`focusout`, event => {
+		if (event.target.matches(`input`)) {
+			const target = event.target;
+			if (target.getAttribute(`name`).match(/^user_(name|message|email|phone)$/)) {
+				target.value = target.value.replace(/^(-| ){1,}|(-| ){1,}$/g, ``);
+				target.value = target.value.replace(/-{2,}/g, `-`);
+				target.value = target.value.replace(/ {2,}/g, ` `);
+			}
+			if (target.getAttribute(`name`) === `user_name`) {
+				// Создаем массив из отдельных слов
+				const words = target.value.match(/[^ |-]{1,}/g);
+				words.forEach((word, index) => {
+					let regWord;
+					// capitalize полученные слова
+					word = word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+					if (index === 0) {
+						regWord = new RegExp(word, `i`);
+					} else if (index === words.length - 1) {
+						regWord = new RegExp(word + `$`, `i`);
+					} else {
+						regWord = new RegExp(`[^а-яё]` + word + `[^а-яё]`, `ig`);
+						const newWord = target.value.match(regWord)[0];
+						word = newWord.slice(0, 2).toUpperCase() + newWord.slice(2).toLowerCase();
+					}
+					target.value = target.value.replace(regWord, word);
+				});
+			}
+		}
+	});
+};
+checkInputData();
 });
